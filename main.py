1fe608da0272d2984e1e5ed53fc168da6316192c
Jaime Ramírez <jaime.ram@gmail.com>
2017-02-19 18:51:51 +0100

    First attempt. Low accuracy

--- main.py
+++ main.py
@@ -4,95 +4,111 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
 from sklearn.linear_model import LogisticRegression
 
-csvfile = open('data/train.csv', newline='')
-
-datareader = csv.DictReader(csvfile)
-
 
 # Most simple params implementation, whether or not a certain work appers in the tweet
 keywords = [
-	"clouds",
-	"cold",
-	"dry",
-	"hot",
-	"humid",
-	"hurricane",
-	"tell",
-	"ice",
-	"other",
-	"rain",
-	"snow",
-	"storms",
-	"sun",
-	"tornado",
-	"wind",
-	"now",
-	"tomorrow",
-	"yesterday",
-	"current"
+    "clouds",
+    "cold",
+    "dry",
+    "hot",
+    "humid",
+    "hurricane",
+    "tell",
+    "ice",
+    "other",
+    "rain",
+    "snow",
+    "storms",
+    "sun",
+    "tornado",
+    "wind",
+    "now",
+    "tomorrow",
+    "yesterday",
+    "current"
 ]
 
 
 X = []
-Y = []
+Y_sentiment = []
+Y_when = []
+Y_type = []
 
 
-print("Preparing data...")
+print("Preparing non numerical data...")
+csvfile = open('data/train.csv', newline='')
+datareader = csv.DictReader(csvfile)
 
 for row in datareader:
-	keywords_in_tweet = []
-	state_in_tweet = 0
-	location_in_tweet = 0
+    keywords_in_tweet = []
+    state_in_tweet = 0
+    location_in_tweet = 0
 
-	# check whether each keyword is inside tweet
-	for word in keywords:
-		if word in row['tweet']:
-			keywords_in_tweet.append(1)
-		else:
-			keywords_in_tweet.append(0)
+    # check whether each keyword is inside tweet
+    for word in keywords:
+        if word in row['tweet']:
+            keywords_in_tweet.append(1)
+        else:
+            keywords_in_tweet.append(0)
 
-	# check whether state is inside tweet
-	if row['state'] in row['tweet']:
-		state_in_tweet = 1
-	
-	# check whether location is inside tweet
-	if row['location'] in row['tweet']:
-		location_in_tweet = 1
-
-	# now generate the numeric arrays x and y
-	x_row = keywords_in_tweet + [state_in_tweet, location_in_tweet]
-	y_row = [
-		row['s1'], row['s2'], row['s3'], row['s4'], row['s5'], 
-		row['w1'], row['w2'], row['w3'], row['w4'], 
-		row['k1'], row['k2'], row['k3'], row['k4'], row['k5'], row['k6'], row['k7'], row['k8'],
-		row['k9'], row['k10'], row['k11'], row['k12'], row['k13'], row['k14'], row['k15']          
-	]
-
-	y_row = [ round(float(val)) for val in y_row ]
-
-	X.append(x_row)
-	Y.append(y_row)
+    # check whether state is inside tweet
+    if row['state'] in row['tweet']:
+        state_in_tweet = 1
+    
+    # check whether location is inside tweet
+    if row['location'] in row['tweet']:
+        location_in_tweet = 1
 
 
+    # each row must have 3 classes: sentiment, when and type
+    # we found the class of each row by looking a the one with max value
+    y_sentiment_classes = [row['s1'], row['s2'], row['s3'], row['s4'], row['s5']]
+    y_when_classes = [row['w1'], row['w2'], row['w3'], row['w4']]
+    y_type_classes = [row['k1'], row['k2'], row['k3'], row['k4'], row['k5'], row['k6'], row['k7'], row['k8'],
+        row['k9'], row['k10'], row['k11'], row['k12'], row['k13'], row['k14'], row['k15']]
+    # we sum 1 to have 1-indexed classes, e.g 1 equals s1
+    y_sentiment = y_sentiment_classes.index(max(y_sentiment_classes)) + 1
+    y_when = y_when_classes.index(max(y_when_classes)) + 1
+    y_type = y_type_classes.index(max(y_type_classes)) + 1
+
+    # now generate the numeric arrays x and y
+    x_row = keywords_in_tweet + [state_in_tweet, location_in_tweet, y_sentiment, y_when, y_type]
+    X.append(x_row)
+
+
+print("Converting data to numpy matrix")
 X = np.matrix(X)
-Y = np.array(Y)
+#Y_sentiment = np.array(Y_sentiment)
+#Y_when = np.array(Y_when)
+#Y_type = np.array(Y_type)
 
+
+print("Shuffling data...")
 np.random.shuffle(X)
-np.random.shuffle(Y)
 
 
-
+print("Splitting data set in training, validation and test sets")
 m = X.shape[0]
 validationset_start = round(m * 0.6)
 testset_start = round(m * 0.8)
-
-
 X_train = X[0:validationset_start, :]
-Y_train = Y[0:validationset_start, :]
 X_validation = X[validationset_start:testset_start, :]
-Y_validation = Y[validationset_start:testset_start, :]
 X_test = X[testset_start:, :]
-Y_test = Y[testset_start:, :]
+
+
+print("Separating features(X) and classes(Y)")
+
+def separate_X_and_Y(X):
+    m = X.shape[0]
+    n = X.shape[1]
+    Y_sentiment = np.ravel(X[:, n-3])
+    Y_when = np.ravel(X[:, n-2])
+    Y_type = np.ravel(X[:, n-1])
+    X = X[:, 0:n-3]
+    return X, Y_sentiment, Y_when, Y_type
+
+X_train, Y_sentiment_train, Y_when_train, Y_type_train = separate_X_and_Y(X_train)
+X_validation, Y_sentiment_validation, Y_when_validation, Y_type_validation = separate_X_and_Y(X_validation)
 
 
 print("Training set size")
@@ -102,14 +118,28 @@ print(X_validation.shape)
 print("Test set size")
 print(X_test.shape)
 
-# Create a binary array marking values as True or False
-from sklearn.preprocessing import MultiLabelBinarizer
-Y_train = MultiLabelBinarizer().fit_transform(Y_train)
+print("Training...")    
+lr_type = LogisticRegression()
+lr_type.fit(X_train, np.ravel(Y_type_train))
 
 
+print("Validating...")   
 
-print("Training...") 	
-print(Y_train)
-lr = LogisticRegression(solver='newton-cg')
-lr.fit(X_train, Y_train)
+def compute_error(X, Y):
+    error = 0;
+    m = X.shape[0]
+    for i in range(0, m - 1):
+        prediction = lr_type.predict(X[i, :]).item(0)
+        y_valid = Y[i]
+        if prediction != y_valid:
+            error = error + 1
+    print('Total examples:', m)
+    print('Total errors:', error)
+    print('Cuadratic mean error:', (error / m))
 
+
+print('- TRAINING SET ERROR')
+compute_error(X_train, Y_type_train)
+
+print('- VALIDATION SET ERROR')
+compute_error(X_validation, Y_type_validation)
